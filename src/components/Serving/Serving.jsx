e5325e2241818d97d3a1b489e4cc258ac779a2e9
Yashwant <yashwant317201@gmail.com>
2023-05-02 16:36:30 +0530

    yo

--- src/components/Serving/Serving.jsx
+++ src/components/Serving/Serving.jsx
@@ -1,36 +1,42 @@
-import React, { useEffect, useState } from 'react'
-import MotionWrap from '../../wrapper/MotionWrap';
+import React, { useEffect, useState } from "react";
+import MotionWrap from "../../wrapper/MotionWrap";
 
 const Serving = () => {
-    const [width, setWidth] = useState(0);
+  const [width, setWidth] = useState(0);
 
-    useEffect(() => {
-        function handleResize() {
-        setWidth(window.innerWidth);
-        }
+  useEffect(() => {
+    function handleResize() {
+      setWidth(window.innerWidth);
+    }
 
-        window.addEventListener("resize", handleResize);
-        handleResize();
+    window.addEventListener("resize", handleResize);
+    handleResize();
 
-        return () => window.removeEventListener("resize", handleResize);
-    }, []);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
 
-    const height = width < 640 ? 480 : 650;
+  const height = width < 640 ? 480 : 650;
   return (
-    <div className='app__flex mt-32 flex-col gap-8 w-full overflow-hidden'>
-        <h2 className="head-text">
-            Currently <span>Serving</span>
-        </h2>
+    <div className="app__flex mt-32 flex-col gap-8 ">
+      <h2 className="head-text mb-4">
+        Currently <span>Serving</span>
+      </h2>
+      <div className="w-full overflow-hidden">
         <iframe
-            title="gMaps"
-            src="https://www.google.com/maps/d/u/0/embed?mid=19kkeX5tHnoNqDZFVB1i3MmxQ8hIIlok&ehbc=2E312F"
-            width={width/1.2}
-            height={height}
-            frameborder="0"
-            style={{border: "2px solid black", marginTop: "-150px"}}
-          ></iframe>
+          title="gMaps"
+          src="https://www.google.com/maps/d/u/0/embed?mid=19kkeX5tHnoNqDZFVB1i3MmxQ8hIIlok&ehbc=2E312F"
+          width={width / 1.2}
+          height={height}
+          frameborder="0"
+          style={{
+            border: "2px solid var(--secondary-color)",
+            marginTop: "-150px",
+            padding: "4px",
+          }}
+        ></iframe>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default MotionWrap(Serving, "app__serving");
