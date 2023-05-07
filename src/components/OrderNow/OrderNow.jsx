6f19c02449cf7919ce23d0c7462f4f9394164126
Yashwant <yashwant317201@gmail.com>
2023-05-07 18:18:21 +0530

    form

--- src/components/OrderNow/OrderNow.jsx
+++ src/components/OrderNow/OrderNow.jsx
@@ -15,15 +15,15 @@ const OrderNow = () => {
   }, []);
 
   const height = width < 650 ? 700 : 450;
-  const width1=width < 650 ? 300 : 700;
+  const width1=width < 650 ? 350 : 700;
 
   return (
     <div className="app__flex flex-col gap-8 w-full h-full">
       <div className="mt-28 h-text">Please fill up this Order Form</div>
-      <div className=" border-black border-2 rounded-xl p-4 mb-8">
+      <div className=" border-black border-2 rounded-xl p-1 py-4 lg:pb-5 mb-8">
         <iframe
           title="gForm"
-          src="https://docs.google.com/forms/d/e/1FAIpQLSedDAMzrU0n-pSiTTGYLR2yrypYPq6u1leZikkHzM-nfRDBCA/viewform?embedded=true"
+          src="https://docs.google.com/forms/d/e/1FAIpQLSdjlCzTpoHIlV-op00vN2WlTO72Z91fmLSFPVVr84WU-Qu76Q/viewform?embedded=true"
           width={width1}
           height={height}
           frameBorder="0"
